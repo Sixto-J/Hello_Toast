edab3a0b158ac9ac5087edd709001a1d41dbf35d
Sixto-J <jvera@cifpfbmoll.eu>
2024-11-07 17:17:17 +0100

    Practica con count y toast 1.0

--- app/src/main/java/com/example/Hello_Toast/MainActivity.java
+++ app/src/main/java/com/example/Hello_Toast/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.Hello_Toast;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,20 +17,38 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        Button btnEnviar = findViewById(R.id.button2);
-        btnEnviar.setOnClickListener(new View.OnClickListener() {
+
+        Button btnEnviar1 = findViewById(R.id.button);
+
+        btnEnviar1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // código a ejecutar cuando sea pulsado
-                TextView tv1 = (TextView)findViewById(R.id.textView);
-                int numerodecontador = Integer.parseInt((findViewById(R.id.textView)).toString().replaceAll("[\\D]", ""));
+                TextView tv1 = findViewById(R.id.textView);
+                String text = tv1.getText().toString();
+                Toast.makeText(MainActivity.this, "TU PUNTUACION ES "+text, 4).show();
+
+
+            }
+        });
+
+        Button btnEnviar2 = findViewById(R.id.button2);
+        btnEnviar2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // código a ejecutar cuando sea pulsado
+                TextView tv1 = findViewById(R.id.textView);
+                String aux_string = tv1.getText().toString();
+                int numerodecontador = Integer.parseInt(aux_string);
+                //.replaceAll("[\\D]", ""));
                 numerodecontador++;
                 tv1.setText(String.valueOf(numerodecontador));
             }
         });
+
 
 
     }
