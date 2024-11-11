1693d49895d6718e1bcb53af004fb78c43ccdb6d
jvera <jvera@cifpfbmoll.eu>
2024-11-11 04:22:42 +0100

    Implementado el guardado del estado para los cambios de orientación

--- app/src/main/java/com/example/Hello_Toast/MainActivity.java
+++ app/src/main/java/com/example/Hello_Toast/MainActivity.java
@@ -1,15 +1,11 @@
 package com.example.Hello_Toast;
 
-import static android.content.ContentValues.TAG;
-
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,43 +13,48 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //EdgeToEdge.enable(this);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
 
         Button btnEnviar1 = findViewById(R.id.button);
 
-        btnEnviar1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // código a ejecutar cuando sea pulsado
-                TextView tv1 = findViewById(R.id.textView);
-                String text = tv1.getText().toString();
-                Toast.makeText(MainActivity.this, "TU PUNTUACION ES "+text, Toast.LENGTH_SHORT).show();
-            }
+        btnEnviar1.setOnClickListener(v -> {
+            // código a ejecutar cuando sea pulsado
+            TextView tv1 = findViewById(R.id.textView);
+            String text = tv1.getText().toString();
+            Toast.makeText(MainActivity.this, "TU PUNTUACION ES "+text, Toast.LENGTH_SHORT).show();
         });
 
         Button btnEnviar2 = findViewById(R.id.button2);
-        btnEnviar2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // código a ejecutar cuando sea pulsado
-                TextView tv1 = findViewById(R.id.textView);
-                String aux_string = tv1.getText().toString();
-                int numerodecontador = Integer.parseInt(aux_string);
-                //.replaceAll("[\\D]", ""));
-                numerodecontador++;
-                tv1.setText(String.valueOf(numerodecontador));
-            }
+        btnEnviar2.setOnClickListener(v -> {
+            // código a ejecutar cuando sea pulsado
+            TextView tv1 = findViewById(R.id.textView);
+            String aux_string = tv1.getText().toString();
+            int numerodecontador = Integer.parseInt(aux_string);
+            //.replaceAll("[\\D]", ""));
+            numerodecontador++;
+            tv1.setText(String.valueOf(numerodecontador));
         });
-
-
-
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        TextView tv1 = findViewById(R.id.textView);
+        outState.putString("YourTextViewTextIdentifier", tv1.getText().toString());
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        TextView tv1 = findViewById(R.id.textView);
+        tv1.setText(savedInstanceState.getString("YourTextViewTextIdentifier"));
+    }
+
 }
 
 
 /*cambiar a RelativeLayout i despres probar ContraintLayout
-
 
 */
